7baa263a6f2eb49b334bfdd8be3cb31abf2aba26
kcsahoo123 <kriss.sahoo@capgemini.com>
2023-01-13 08:01:33 +0000

    changes

---
+++ RACE2/Race2.IdentityProvider/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Race2.IdentityProvider.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
