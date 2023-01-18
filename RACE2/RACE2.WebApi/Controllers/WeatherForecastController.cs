696abdf3d663beac71c1c97d69690ad582d61ebd
kcsahoo123 <kriss.sahoo@capgemini.com>
2023-01-18 11:13:11 +0000

    aspnet core identity objects made visible

--- RACE2/RACE2.WebApi/Controllers/WeatherForecastController.cs
+++ RACE2/RACE2.WebApi/Controllers/WeatherForecastController.cs
@@ -6,7 +6,7 @@ namespace RACE2.WebApi.Controllers
     [ApiController]
     [Route("[controller]")]
     //[Authorize(Roles ="engineer")]
-    //[Authorize()]
+    [Authorize()]
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
