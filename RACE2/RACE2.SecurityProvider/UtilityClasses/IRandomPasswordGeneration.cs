cd8ca30ee91bf96fc3f72bec657636bfd58ee745
kcsahoo123 <kriss.sahoo@capgemini.com>
2023-01-20 11:21:58 +0000

    latest code

---
+++ RACE2/RACE2.SecurityProvider/UtilityClasses/IRandomPasswordGeneration.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace RACE2.SecurityProvider.UtilityClasses
+{
+    public interface IRandomPasswordGeneration
+    {
+        public string GenerateRandomPassword(PasswordOptions opts);
+    }
+}
