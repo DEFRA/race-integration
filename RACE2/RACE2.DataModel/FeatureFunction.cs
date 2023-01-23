169966f01cff7cef7c9a526791ee3644e2d5c543
Mahalakshmi Alagarsamy <mahalakshmi.alagarsamy@capgemini.com>
2023-01-23 10:39:59 +0000

    Added Data models to the User Table

---
+++ RACE2/RACE2.DataModel/FeatureFunction.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RACE2.DataModel
+{
+    public class FeatureFunction
+    {
+        [Key, Required]
+        public int id { get; set; }
+        [StringLength(64)]
+        [Required]
+        public string name { get; set; }
+        [StringLength(64)]
+        public string? display_name { get; set; }
+        [StringLength(64)]
+        public string? description { get; set; }
+
+        [StringLength(64)]
+        public string? default_value { get; set; }
+        public DateTime start_date { get; set; }
+        public DateTime end_date { get; set; }
+    }
+}
---
+++ RACE2/RACE2.DataModel/Roles.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RACE2.DataModel
+{
+    public class Roles : IdentityRole
+    {
+        public string? display_name { get; set; }
+        public string? description { get; set; }         
+        public int parent_id { get;set; }
+        public DateTime start_date { get; set; }
+        public DateTime end_date { get; set; }
+    }
+}
