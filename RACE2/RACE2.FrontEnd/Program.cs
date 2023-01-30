abb0c703705d2086a44f437a2301292bf2b7f709
kcsahoo123 <kriss.sahoo@capgemini.com>
2023-01-30 11:58:45 +0000

    latest

--- RACE2/RACE2.FrontEnd/Program.cs
+++ RACE2/RACE2.FrontEnd/Program.cs
@@ -9,17 +9,12 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-builder.Services.AddScoped<DialogService>();
-builder.Services.AddScoped<NotificationService>();
-builder.Services.AddScoped<TooltipService>();
-builder.Services.AddScoped<ContextMenuService>();
-
-builder.Services.AddOidcAuthentication(options =>
-{
-    // Configure your authentication provider options here.
-    // For more information, see https://aka.ms/blazor-standalone-auth
-    builder.Configuration.Bind("oidc", options.ProviderOptions);
-    options.UserOptions.RoleClaim = "role";
-});
+//builder.Services.AddOidcAuthentication(options =>
+//{
+//    // Configure your authentication provider options here.
+//    // For more information, see https://aka.ms/blazor-standalone-auth
+//    builder.Configuration.Bind("oidc", options.ProviderOptions);
+//    options.UserOptions.RoleClaim = "role";
+//});
 
 await builder.Build().RunAsync();
