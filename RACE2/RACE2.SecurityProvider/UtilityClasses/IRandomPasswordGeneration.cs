7b74e5337daa4981cca1f47fd5e5a906152ca312
kcsahoo123 <kriss.sahoo@capgemini.com>
2023-01-31 08:43:01 +0000

    reverted

--- RACE2/RACE2.SecurityProvider/UtilityClasses/IRandomPasswordGeneration.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace RACE2.SecurityProvider.UtilityClasses
-{
-    public interface IRandomPasswordGeneration
-    {
-        public string GenerateRandomPassword(PasswordOptions opts);
-    }
-}
