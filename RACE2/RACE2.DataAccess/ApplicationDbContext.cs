555a1753a84d0492c783d27118e5c062c7c98e04
kcsahoo123 <kriss.sahoo@capgemini.com>
2023-01-31 12:10:16 +0000

    final changes

--- RACE2/RACE2.DataAccess/ApplicationDbContext.cs
+++ RACE2/RACE2.DataAccess/ApplicationDbContext.cs
@@ -9,19 +9,13 @@ using System.IO;
 
 namespace RACE2.DataAccess
 {
-    //public class ApplicationDbContext : IdentityDbContext<Userdetails, Roles,string>
-    public class ApplicationDbContext : IdentityDbContext<Userdetails>
-    {
-        IConfigurationRoot _configuration;
+    public class ApplicationDbContext : IdentityDbContext<Userdetails, Roles,string>
+     {
+        
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {            
-        }
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options, IConfigurationRoot configuration)
-            : base(options)
-        {
-            _configuration = configuration;
-        }
+        }       
 
         public DbSet<FeatureFunction> FeatureFunction { get; set; }
 
