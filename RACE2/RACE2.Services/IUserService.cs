231a9fcf19788066e71ac27f0b8e5bb21dbdda3f
Mahalakshmi Alagarsamy <mahalakshmi.alagarsamy@capgemini.com>
2023-02-01 11:47:36 +0000

    Updated Service classes

---
+++ RACE2/RACE2.Services/IUserService.cs
@@ -0,0 +1,16 @@
+﻿using RACE2.DataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RACE2.Services
+{
+    public interface IUserService
+    {
+        public Task<IEnumerable<Userdetails>> GetUserdetails();
+        //   public Task<Userdetails> GetUser(int id);
+        public Task<Userdetails> CreateUser(Userdetails newuser);
+    }
+}
---
+++ RACE2/RACE2.Services/UserService.cs
@@ -0,0 +1,30 @@
+﻿using RACE2.DataAccess.Repository;
+using RACE2.DataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+
+namespace RACE2.Services
+{
+    public class UserService : IUserService
+    {
+        public IUserRepository _userRepository;
+        public UserService(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+        public async Task<IEnumerable<Userdetails>> GetUserdetails()
+        {
+            return await _userRepository.GetUserdetails();
+        }
+
+        public async Task<Userdetails> CreateUser(Userdetails newuser)
+        {
+           return await _userRepository.CreateUser(newuser);
+        }
+    }
+
+}
