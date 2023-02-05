bbb470968f2a5c671912f7e6ed07ee52d18491bf
kcsahoo123 <kriss.sahoo@capgemini.com>
2023-02-05 09:42:23 +0000

    strawberryshake added

--- RACE2/RACE2.FrontEnd/Program.cs
+++ RACE2/RACE2.FrontEnd/Program.cs
@@ -8,6 +8,9 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+builder.Services.AddRace2GraphqlClient()
+    .ConfigureHttpClient(client => client.BaseAddress = new Uri("https://localhost:5004/graphql/"));
+
 //builder.Services.AddOidcAuthentication(options =>
 //{
 //    // Configure your authentication provider options here.
