a3cb1f266b6aa62c9b97f52f1dc1165b067ddc28
kcsahoo123 <kriss.sahoo@capgemini.com>
2023-02-06 16:45:09 +0000

    checked

---
+++ RACE2/RACE2.FrontEnd/ApplicationSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace RACE2.FrontEnd
+{
+    public class ApplicationSettings
+    {
+        public string AllowedHosts { get; set; }
+        public string BlazorClientURL { get; set; }
+        public string BlazorWebApiURL { get; set; }
+        public string BlazorSecurityProviderURL { get; set; }
+    }
+}
