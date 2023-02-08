06d13ea70445522b53021e5a6a2e588db3c39feb
kcsahoo123 <kriss.sahoo@capgemini.com>
2023-02-08 07:44:16 +0000

    unit test

---
+++ RACE2/RACE2.UnitTest/GetUserdetails.cs
@@ -0,0 +1,31 @@
+using Microsoft.Extensions.Configuration;
+using RACE2.DataAccess.Repository;
+using System.IO;
+
+namespace RACE2.UnitTest
+{
+    public class GetUserdetails
+    {
+        public GetUserdetails()
+        {
+        }
+        [Fact]
+        public void GetFeatureFunctionTest()
+        {
+            var config = InitConfiguration();
+            var connectionString = config.GetConnectionString("DefaultConnection");
+            UserRepository repo = new UserRepository(config);
+            //var details = repo.GetFeatureFunctions(config);
+            var details = repo.GetUserdetails();
+            Assert.NotNull(details);
+        }
+
+        public static IConfiguration InitConfiguration()
+        {
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+            return config;
+        }
+    }
+}
