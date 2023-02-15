b2bd097357cecadc9e75d6541140ed0991f3d53a
kcsahoo123 <kriss.sahoo@capgemini.com>
2023-02-15 11:16:39 +0000

    config changes

---
+++ RACE2/RACE2.DataAccess/AppConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RACE2.DataAccess
+{
+    public class AppConfiguration
+    {
+        public string AllowedHosts { get; set; }
+        public string RACE2FrontEndURL { get; set; }
+        public string RACE2WebApiURL { get; set; }
+        public string RACE2SecurityProviderURL { get; set; }
+    }
+}
