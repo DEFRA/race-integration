51ecae49fbbe59e1ec64a48fb38be9e9d450ba3e
kcsahoo123 <kriss.sahoo@capgemini.com>
2023-02-15 15:03:16 +0000

    state management frontend

--- RACE2/RACE2.FrontEnd/Program.cs
+++ RACE2/RACE2.FrontEnd/Program.cs
@@ -2,18 +2,20 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using RACE2.FrontEnd;
 using Microsoft.Extensions.Configuration;
+using RACE2.FrontEnd.StateObjects;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-var http = new HttpClient()
+var httpClient = new HttpClient()
 {
     BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
 };
-builder.Services.AddScoped(sp => http);
+builder.Services.AddScoped(sp => httpClient);
+builder.Services.AddScoped<AppState>();
 
-using var configSettings = await http.GetAsync("settings.json");
+using var configSettings = await httpClient.GetAsync("settings.json");
 using var stream = await configSettings.Content.ReadAsStreamAsync();
 
 builder.Configuration.AddJsonStream(stream);
---
+++ RACE2/RACE2.FrontEnd/StateObjects/AppState.cs
@@ -0,0 +1,38 @@
+﻿namespace RACE2.FrontEnd.StateObjects
+{
+    public class AppState
+    {
+        public string Email { get; set; }
+        public string ReservoirName { get; set; }
+
+        private string _currentEmail = String.Empty;
+        private string _currentReservoirName = String.Empty;
+
+        public event EventHandler StateChangeHandler;
+
+        private void StateHasChanged()
+        {
+            StateChangeHandler?.Invoke(this, EventArgs.Empty);
+        }
+
+        public string GetEmail()
+        {
+            return _currentEmail;
+        }
+        public void SetEmail(string email)
+        {
+            _currentEmail = email;
+            StateHasChanged();
+        }
+
+        public string GetReservoirName()
+        {
+            return _currentReservoirName;
+        }
+        public void SetReservoirName(string reservoirName)
+        {
+            _currentReservoirName = reservoirName;
+            StateHasChanged();
+        }
+    }
+}
