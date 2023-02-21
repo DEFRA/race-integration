1b63d4869bc44148d92c8631363ad0861665619c
kcsahoo123 <kriss.sahoo@capgemini.com>
2023-02-21 19:28:45 +0000

    chages

--- RACE2/RACE2.SecurityProvider/Program.cs
+++ RACE2/RACE2.SecurityProvider/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations.Internal;
 using RACE2.DataAccess;
 using RACE2.DataModel;
 using RACE2.SecurityProvider;
@@ -8,6 +9,7 @@ using RACE2.SecurityProvider.UtilityClasses;
 var builder = WebApplication.CreateBuilder(args);
 IConfiguration _configuration = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile(@Directory.GetCurrentDirectory() + "/../appsettings.json").Build();
 string RACE2FrontEndURL = _configuration["ApplicationSettings:RACE2FrontEndURL"];
+var migrationsAssembly = typeof(Program).Assembly.GetName().Name;
 
 // Add services to the container.
 var connectionString = _configuration.GetConnectionString("DefaultConnection");
@@ -18,12 +20,17 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddDefaultIdentity<UserDetail>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddRazorPages();
-var serverConfiguration = new ServerConfiguration();
 builder.Services.AddIdentityServer()
-            .AddInMemoryApiResources(serverConfiguration.ApiResources)
-            .AddInMemoryApiScopes(serverConfiguration.ApiScopes)
-            .AddInMemoryIdentityResources(serverConfiguration.IdentityResources)
-            .AddInMemoryClients(serverConfiguration.Clients(RACE2FrontEndURL))
+            .AddConfigurationStore(options =>
+            {
+                options.ConfigureDbContext = b => b.UseSqlServer(connectionString,
+                    sql => sql.MigrationsAssembly(migrationsAssembly));
+            })
+            .AddOperationalStore(options =>
+            {
+                options.ConfigureDbContext = b => b.UseSqlServer(connectionString,
+                    sql => sql.MigrationsAssembly(migrationsAssembly));
+            })
             .AddDeveloperSigningCredential()
             .AddAspNetIdentity<UserDetail>();
 builder.Services.AddScoped<IRandomPasswordGeneration, RandomPasswordGeneration>();
