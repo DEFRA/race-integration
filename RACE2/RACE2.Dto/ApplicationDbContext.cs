f08ad932d50dd1fa5fb0bec7ce848de0619d51a7
kcsahoo123 <kriss.sahoo@capgemini.com>
2023-03-28 17:03:13 +0100

    change

--- RACE2/RACE2.Dto/ApplicationDbContext.cs
+++ RACE2/RACE2.Dto/ApplicationDbContext.cs
@@ -194,15 +194,15 @@ namespace RACE2.Dto
         }
     }
 
-    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
-    {
-        public ApplicationDbContext CreateDbContext(string[] args)
-        {
-            IConfigurationRoot configuration = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile(@Directory.GetCurrentDirectory() + "/../appsettings.json").Build();
-            var builder = new DbContextOptionsBuilder<ApplicationDbContext>();
-            var connectionString = configuration.GetConnectionString("DefaultConnection");
-            builder.UseSqlServer(connectionString);
-            return new ApplicationDbContext(builder.Options);
-        }
-    }
+    //public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    //{
+    //    public ApplicationDbContext CreateDbContext(string[] args)
+    //    {
+    //        IConfigurationRoot configuration = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile(@Directory.GetCurrentDirectory() + "/../appsettings.json").Build();
+    //        var builder = new DbContextOptionsBuilder<ApplicationDbContext>();
+    //        var connectionString = configuration.GetConnectionString("DefaultConnection");
+    //        builder.UseSqlServer(connectionString);
+    //        return new ApplicationDbContext(builder.Options);
+    //    }
+    //}
 }
