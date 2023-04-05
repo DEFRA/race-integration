6917c73de29bef63bf08a3f02071ee766e781f5d
kcsahoo123 <kriss.sahoo@capgemini.com>
2023-04-05 11:54:06 +0100

    latest changes

--- RACE2/WebApiDeploy.ps1
+++ RACE2/WebApiDeploy.ps1
@@ -1,11 +1,16 @@
 $RESOURCE_GROUP = "race2projectrg"
 $LOCATION="westeurope"
+$ENVIRONMENT="Production"
+$MANAGEDIDENTITY="Race2ManagedIdentity"
 $CONTAINERAPPS_ENVIRONMENT="race2containerappenv"
 $CONTAINERAPPNAME ="race2webapi"
 $REMOTE_IMAGENAME="race2acr.azurecr.io/race2webapi:v1"
 $REGISTRY_SERVER="race2acr.azurecr.io"
 $REGISTRY_USERNAME="Race2ACR"
 $REGISTRY_PASSWORD="3q23FKncYFoNMky5N+4arugBI6FHagtWC07sVgPHdo+ACRB3HwHE"  
+$APPCONFIG_URL="https://race2appconfig.azconfig.io/"
+
+$identityClientId = (az identity show --resource-group $RESOURCE_GROUP --name $MANAGEDIDENTITY --output json --query "clientId")
 
 az containerapp create `
   --name $CONTAINERAPPNAME `
@@ -21,6 +26,6 @@ az containerapp create `
   --memory 1.0Gi `
   --min-replicas 0 `
   --max-replicas 2 `
-  --env-vars ASPNETCORE_ENVIRONMENT="Production" AzureAppConfigURL="https://race2appconfig.azconfig.io/" AZURE_CLIENT_ID="f324da0d-19e1-49df-8901-faab72ed2649" `
-  --user-assigned "Race2ManagedIdentity"
+  --env-vars ASPNETCORE_ENVIRONMENT=$ENVIRONMENT AzureAppConfigURL=$APPCONFIG_URL AZURE_CLIENT_ID=$identityClientId `
+  --user-assigned $MANAGEDIDENTITY
  
