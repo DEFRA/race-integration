97f5a57c807ea438c3b0ca094f13e6c96de570fd
kcsahoo <kcsahoo@gmail.com>
2023-05-13 11:58:35 +0100

    FrontEndWebProject updated

---
+++ RACE2/RACE2.FrontEndWeb/Components/Home.razor.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace RACE2.FrontEndWeb.Components
+{
+    public partial class Home
+    {
+        [Inject]
+        public NavigationManager NavigationManager { get; set; } = default!;
+
+        private IEnumerable<Claim> UserClaims { get; set; }
+        private string UserName { get; set; } = "Unknown";
+
+        protected override async void OnInitialized()
+        {
+            AuthenticationState authState = await AuthenticationStateProvider.GetAuthenticationStateAsync();
+            if (authState is not null)
+            {
+                NavigationManager.NavigateTo("/choose-a-reservoir", true);
+            }
+        }
+        public async void GoToNextPage()
+        {
+            bool forceLoad = true;
+            //AuthenticationState authState = await AuthenticationStateProvider.GetAuthenticationStateAsync();
+            NavigationManager.NavigateTo("/authentication/login", forceLoad);
+            //if (authState is null)
+            //{
+            //    NavigationManager.NavigateTo("/authentication/login", forceLoad);
+            //}
+            //else
+            //{
+            //    UserName = authState.User.Identity.Name;
+            //    UserClaims = authState.User.Claims;
+
+            //    NavigationManager.NavigateTo("/choose-a-reservoir", forceLoad);
+            //}            
+        }
+    }
+}
--- RACE2/RACE2.FrontEndWeb/Components/ReservoirNotListed.razor.cs
+++ RACE2/RACE2.FrontEndWeb/Components/ReservoirNotListed.razor.cs
@@ -3,40 +3,36 @@ using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Identity;
 using RACE2.DataModel;
 using RACE2.Dto;
-using RACE2.FrontEndWeb.FluxorImplementation.Actions;
 using RACE2.FrontEndWeb.FluxorImplementation.Stores;
-using RACE2.FrontEndWeb.RACE2GraphQLSchema;
+//using RACE2.FrontEndWeb.RACE2GraphQLSchema;
 
 namespace RACE2.FrontEndWeb.Components
 {
     public partial class ReservoirNotListed
     {
 
-        [Inject]
-        public RACE2GraphQLClient client { get; set; } = default!;
+        //[Inject]
+        //public RACE2GraphQLClient client { get; set; } = default!;
         [Inject]
         public NavigationManager NavigationManager { get; set; } = default!;
-
         [Inject]
-        public IState<AppStore> State { get; set; } = default!;
+        public IState<CurrentUserDetailState> State { get; set; } = default!;
 
         [Inject]
         public IDispatcher Dispatcher { get; set; } = default!;
 
-        public AppStore AppStore => State.Value;
+        public CurrentUserDetailState CurrentUserDetailState => State.Value;
 
         public ReservoirDetailsDTO newReservoir =new ReservoirDetailsDTO();
 
         protected override void OnInitialized()
         {
-            newReservoir = AppStore.NewReservoirDetails;
+            //newReservoir = CurrentUserDetailState.NewReservoirDetails;
         }
 
         bool forceLoad = true;
         public void GoToNextPage()
         {
-            var action = new StoreNewReservoirAction(newReservoir);
-            Dispatcher.Dispatch(action);
             NavigationManager.NavigateTo("/new-reservoir", forceLoad);
         }
     }
