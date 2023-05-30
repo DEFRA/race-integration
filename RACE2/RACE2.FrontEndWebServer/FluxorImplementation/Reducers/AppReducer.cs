f9349d7b2d01ef3c43f60105713f7a6c4421ec82
kcsahoo123 <kriss.sahoo@capgemini.com>
2023-05-30 12:00:59 +0100

    added new blazor server project RACE2.FrontEndWebServer

---
+++ RACE2/RACE2.FrontEndWebServer/FluxorImplementation/Reducers/AppReducer.cs
@@ -0,0 +1,31 @@
+﻿using Fluxor;
+using RACE2.FrontEndWebServer.FluxorImplementation.Actions;
+using RACE2.FrontEndWebServer.FluxorImplementation.Stores;
+
+namespace RACE2.FrontEndWebServer.FluxorImplementation.Reducers
+{
+    public static class AppReducer
+    {
+        [ReducerMethod]
+        public static CurrentUserDetailState ReduceStoreUserDetailAction(CurrentUserDetailState state, StoreUserDetailAction action)
+             => new CurrentUserDetailState(currentUserDetail:action.CurrentUserDetail);
+
+        [ReducerMethod]
+        public static CurrentReservoirState ReduceStoreReservoirAction(CurrentReservoirState state, StoreReservoirAction action)
+             => new CurrentReservoirState(currentReservoir: action.CurrentReservoir);
+
+        //[ReducerMethod]
+        //public static AppStore ReduceStoreNewReservoirAction(AppStore state, StoreNewReservoirAction action)
+        //     => state with { 
+        //         IsLoading = false, 
+        //         NewReservoirDetails = action.NewReservoirDetails 
+        //     };
+
+        [ReducerMethod]
+        public static AppStore ReduceStoreIsLoggedInAction(AppStore state, StoreIsLoggedInAction action)
+             => state with { 
+                 IsLoggedIn = action.IsLoggedIn 
+             };
+
+    }
+}
