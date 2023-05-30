a92655d13bba4de38f1af804f98bba132061fb45
kcsahoo123 <kriss.sahoo@capgemini.com>
2023-05-30 12:51:44 +0100

    new project changes

---
+++ RACE2/RACE2.FrontEndWebServer/Pages/S12Pages/NonStat.razor.cs
@@ -0,0 +1,6 @@
+﻿namespace RACE2.FrontEndWebServer.Pages.S12Pages
+{
+    public partial class NonStat
+    {
+    }
+}
---
+++ RACE2/RACE2.FrontEndWebServer/Pages/S12Pages/ReservoirDetails.razor.cs
@@ -0,0 +1,50 @@
+﻿using Fluxor;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Identity;
+using RACE2.DataModel;
+using RACE2.FrontEndWebServer.FluxorImplementation.Stores;
+using RACE2.FrontEndWebServer.FluxorImplementation.Actions;
+using RACE2.FrontEndWebServer.RACE2GraphQLSchema;
+
+namespace RACE2.FrontEndWebServer.Pages.S12Pages
+{
+    public partial class ReservoirDetails
+    {
+        [Inject]
+        public RACE2GraphQLClient client { get; set; } = default!;
+        [Inject]
+        public NavigationManager NavigationManager { get; set; } = default!;
+        [Inject]
+        public IState<CurrentReservoirState> CurrentReservoirState { get; set; } = default!;
+
+        [Inject]
+        public IDispatcher Dispatcher { get; set; } = default!;
+
+        public Reservoir CurrentReservoir { get; set; } = new Reservoir();
+        public string ReservoirName { get; set; } = default!;
+
+        protected override async void OnInitialized()
+        {
+            AuthenticationState authState = await AuthenticationStateProvider.GetAuthenticationStateAsync();
+            base.OnInitialized();
+        }
+
+        public async void GoToNextPage()
+        {
+            NavigationManager.NavigateTo("/confirm-operator", true);
+        }
+
+        public async void GoToSaveComebackLaterPage()
+        {
+
+        }
+
+        private void goback()
+        {
+            bool forceLoad = false;
+            string pagelink = "/choose-a-reservoir";
+            NavigationManager.NavigateTo(pagelink, forceLoad);
+        }        
+    }
+}
