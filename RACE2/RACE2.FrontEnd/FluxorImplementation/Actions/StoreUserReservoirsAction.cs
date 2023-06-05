1f17ab49d8052ae3de5650f524bdc1e9ded1c48f
kcsahoo123 <kriss.sahoo@capgemini.com>
2023-06-05 15:22:40 +0100

    fluxor changes

---
+++ RACE2/RACE2.FrontEnd/FluxorImplementation/Actions/StoreUserReservoirsAction.cs
@@ -0,0 +1,11 @@
+﻿using RACE2.DataModel;
+
+namespace RACE2.FrontEnd.FluxorImplementation.Actions
+{
+    public class StoreUserReservoirsAction
+    {
+        public List<Reservoir> UserReservoirs { get; }
+        public StoreUserReservoirsAction(List<Reservoir> userReservoirs)
+            => UserReservoirs = userReservoirs;
+    }
+}
