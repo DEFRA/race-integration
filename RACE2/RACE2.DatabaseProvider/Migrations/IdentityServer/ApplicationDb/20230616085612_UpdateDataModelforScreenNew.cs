c1b6d61c6487f15474f68c7294408c223c671091
Mahalakshmi Alagarsamy <mahalakshmi.alagarsamy@capgemini.com>
2023-06-16 09:56:38 +0100

    Updated

---
+++ RACE2/RACE2.DatabaseProvider/Migrations/IdentityServer/ApplicationDb/20230616085612_UpdateDataModelforScreenNew.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RACE2.DatabaseProvider.Migrations.IdentityServer.ApplicationDb
+{
+    public partial class UpdateDataModelforScreenNew : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "SubmittedBy",
+                table: "SubmissionStatus",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "SubmittedBy",
+                table: "SubmissionStatus",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+    }
+}
