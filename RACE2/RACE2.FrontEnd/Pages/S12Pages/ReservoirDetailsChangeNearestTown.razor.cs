2f5e07e3f61b5360d412a7520c2387ee782b3939
kcsahoo123 <kriss.sahoo@capgemini.com>
2023-06-23 11:57:40 +0100

    added pages

--- RACE2/RACE2.FrontEnd/Pages/S12Pages/ReservoirDetailsChangeNearestTown.razor.cs
+++ RACE2/RACE2.FrontEnd/Pages/S12Pages/ReservoirDetailsChangeNearestTown.razor.cs
@@ -1,13 +1,14 @@
 ﻿using Fluxor;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Identity;
 using RACE2.DataModel;
-using RACE2.FrontEnd.FluxorImplementation.Stores;
+using RACE2.Dto;
 using RACE2.FrontEnd.FluxorImplementation.Actions;
+using RACE2.FrontEnd.FluxorImplementation.Stores;
 using RACE2.FrontEnd.RACE2GraphQLSchema;
-using Microsoft.AspNetCore.Components.Web;
-using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+//using RACE2.FrontEnd.RACE2GraphQLSchema;
 
 namespace RACE2.FrontEnd.Pages.S12Pages
 {
@@ -21,11 +22,30 @@ namespace RACE2.FrontEnd.Pages.S12Pages
         public NavigationManager NavigationManager { get; set; } = default!;
         [Inject]
         public IState<CurrentUserDetailState> CurrentUserDetailState { get; set; } = default!;
- 
+        [Inject]
+        public IState<CurrentReservoirState> CurrentReservoirState { get; set; } = default!;
+
         [Inject]
         public IDispatcher Dispatcher { get; set; } = default!;
 
         public Reservoir CurrentReservoir { get; set; } = new Reservoir();
+        public string ReservoirName { get; set; } = default!;
+
+        protected override async void OnInitialized()
+        {
+            AuthenticationState authState = await AuthenticationStateProvider.GetAuthenticationStateAsync();
+            var currentUser = CurrentUserDetailState.Value.CurrentUserDetail;
+            CurrentReservoir = CurrentReservoirState.Value.CurrentReservoir;
+            base.OnInitialized();
+        }
+
+        bool forceLoad = false;
+        public void GoToNextPage()
+        {
+            var action = new StoreNewReservoirAction(CurrentReservoir);
+            Dispatcher.Dispatch(action);
+            NavigationManager.NavigateTo("/reservoir-details", forceLoad);
+        }
 
         private void goback()
         {
