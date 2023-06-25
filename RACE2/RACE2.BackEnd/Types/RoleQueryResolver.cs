be2f1e31d5fb3de3cb012f9efde2fcf18109e475
kcsahoo123 <122529250+kcsahoo123@users.noreply.github.com>
2023-06-25 13:28:27 +0100

    new backend project

---
+++ RACE2/RACE2.BackEnd/Types/RoleQueryResolver.cs
@@ -0,0 +1,22 @@
+﻿using RACE2.DataModel;
+using RACE2.Services;
+
+namespace RACE2.BackEnd.Types
+{
+    [QueryType]
+    public class RoleQueryResolver
+    {
+        private readonly ILogger<UserResolver> _logger;
+
+        public RoleQueryResolver(ILogger<UserResolver> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<IEnumerable<FeatureFunction>> GetFeaturePermissionForRole(IUserService _userService, int roleid)
+        {
+            var result = await _userService.GetFeaturePermissionForRole(roleid);
+            return result;
+        }
+    }
+}
