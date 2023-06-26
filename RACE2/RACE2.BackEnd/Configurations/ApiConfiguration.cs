b6ef11577f27cc52bad2fb595225e251b9555eea
kcsahoo123 <kriss.sahoo@capgemini.com>
2023-06-26 13:25:13 +0100

    changes

---
+++ RACE2/RACE2.BackEnd/Configurations/ApiConfiguration.cs
@@ -0,0 +1,9 @@
+﻿using RACE2.DataAccess;
+
+namespace RACE2.BackEnd.Configurations
+{
+    public class ApiConfiguration
+    {
+        public AppConfiguration AppConfiguration { get; set; }
+    }
+}
