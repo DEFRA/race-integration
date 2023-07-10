9b5f22f7c9ed87dac73a89db19062fec8764e224
kcsahoo123 <122529250+kcsahoo123@users.noreply.github.com>
2023-07-10 10:51:07 +0100

    change

--- RACE2/RACE2.WebApi/Types/Query/ReservoirQueryResolver.cs
+++ RACE2/RACE2.WebApi/Types/Query/ReservoirQueryResolver.cs
@@ -7,6 +7,11 @@ using DocumentFormat.OpenXml.Packaging;
 using DocumentFormat.OpenXml.Wordprocessing;
 using DocumentFormat.OpenXml;
 using System.IO.Packaging;
+using Azure;
+using GreenDonut;
+using Azure.Storage.Blobs.Models;
+using Azure.Storage.Blobs;
+using System.Text;
 
 namespace RACE2.WebApi.Types
 {
@@ -22,7 +27,7 @@ namespace RACE2.WebApi.Types
 
         public async Task<Reservoir> GetReservoirById(IReservoirService _reservoirService, int id)
         {
-            OpenAndAddTextToWordDocument(@"d:\temp\test1.docx", "Test Doc");
+            OpenAndAddTextToWordDocument();
             var result = await _reservoirService.GetReservoirById(id);
             return result;
         }
@@ -92,26 +97,99 @@ namespace RACE2.WebApi.Types
             }
         }
 
-        public void OpenAndAddTextToWordDocument(string filepath, string txt)
+        public void OpenAndAddTextToWordDocument()
         {
-            // Open a WordprocessingDocument for editing using the filepath.
-            WordprocessingDocument wordprocessingDocument =
-                WordprocessingDocument.Create(filepath, WordprocessingDocumentType.Document);
+            ////Open a WordprocessingDocument for editing using the filepath.
+            //WordprocessingDocument wordprocessingDocument =
+            //    WordprocessingDocument.Create(@"d:\temp\test.docx", WordprocessingDocumentType.Document);
 
-            wordprocessingDocument.AddMainDocumentPart();
+            //wordprocessingDocument.AddMainDocumentPart();
 
-            // Create the Document DOM. 
-            wordprocessingDocument.MainDocumentPart.Document =
-              new Document(
-                new Body(
-                  new Paragraph(
-                    new Run(
-                      new Text("Hello World!")))));
+            //// Create the Document DOM. 
+            //wordprocessingDocument.MainDocumentPart.Document =
+            //  new Document(
+            //    new Body(
+            //      new Paragraph(
+            //        new Run(
+            //          new Text("Hello World!")))));
 
-            // Save changes to the main document part. 
-            wordprocessingDocument.MainDocumentPart.Document.Save();
-            // Close the handle explicitly.
-            wordprocessingDocument.Dispose();
+            //// Save changes to the main document part. 
+            //wordprocessingDocument.MainDocumentPart.Document.Save();
+            //// Close the handle explicitly.
+            //wordprocessingDocument.Dispose();
+
+            //byte[] byteArray = File.ReadAllBytes(@"d:\temp\test.docx");
+            //using (MemoryStream mem = new MemoryStream())
+            //{
+            //    mem.Write(byteArray, 0, (int)byteArray.Length);
+            //    using (WordprocessingDocument wordDoc =
+            //        WordprocessingDocument.Open(mem, true))
+            //    {
+            //        // Modify the document as necessary.
+            //        // For this example, we insert a new paragraph at the
+            //        // beginning of the document.
+            //        wordDoc.MainDocumentPart.Document.Body.InsertAt(
+            //            new Paragraph(
+            //                new Run(
+            //                    new Text("Newly inserted paragraph."))), 0);
+            //    }
+            //    // At this point, the memory stream contains the modified document.
+            //    // We could write it back to a SharePoint document library or serve
+            //    // it from a web server.
+
+            //    // In this example, we serialize back to the file system to verify
+            //    // that the code worked properly.
+            //    using (FileStream fileStream = new FileStream(@"d:\temp\test1.docx",
+            //        System.IO.FileMode.CreateNew))
+            //    {
+            //        mem.WriteTo(fileStream);
+            //    }
+            //}
+
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (WordprocessingDocument wordprocessingDocument = WordprocessingDocument.Create(ms, WordprocessingDocumentType.Document, true))
+                {
+                    MainDocumentPart mainPart = wordprocessingDocument.AddMainDocumentPart();
+                    wordprocessingDocument.MainDocumentPart.Document =
+                     new Document(
+                       new Body(
+                         new Paragraph(
+                           new Run(
+                             new Text("Hello World!")))));
+                    mainPart.Document.Save();
+                }
+
+                //// In this example, we serialize back to the file system to verify that the code worked properly.
+                //using (FileStream fileStream = new FileStream(@"d:\temp\test.docx", System.IO.FileMode.CreateNew))
+                //{
+                //    ms.WriteTo(fileStream);
+                //}
+
+                // connection to be given in configuration files or env variable  - Get value from step 10 given in article
+
+                var connectionString = "DefaultEndpointsProtocol=https;AccountName=race2storageaccount;AccountKey=+voxyaI7i37XXY89mgL3FAg/1JhvSezh1ENdokcV5GMwCOycBYNfYY15aUak3iD+DMvG0Z4kOc6u+ASt0Rq3ZA==;EndpointSuffix=core.windows.net";
+
+                // create a client with the connection
+
+                BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
+
+                // container name which we created
+
+                BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient("race2webapicontainer");
+
+                BlobClient blobClient = containerClient.GetBlobClient("testdata");
+
+                var blobHttpHeader = new BlobHttpHeaders();
+
+                blobHttpHeader.ContentType = "application/vnd.openxmlformats-officedocument.wordprocessingml.document";
+
+                blobClient.UploadAsync(ms, blobHttpHeader); // can use memory stream or file stream or Direct File path
+
+                //return blobClient.Uri.AbsoluteUri;
+
+                }
+            }
         }
-    }
 }
