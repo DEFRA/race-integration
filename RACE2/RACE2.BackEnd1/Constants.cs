51a0cc402b10bde95165a6ccaa3c968c127e7491
kcsahoo123 <122529250+kcsahoo123@users.noreply.github.com>
2023-07-22 13:21:43 +0100

    change

--- RACE2/RACE2.BackEnd1/Constants.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace RACE2.BackEnd
-{
-    public static class Constants
-    {
-        public const string ImageDirectory = "./wwwroot/images";
-        public const string ImageRoot = "http://localhost:5095/images";
-    }
-}
-
