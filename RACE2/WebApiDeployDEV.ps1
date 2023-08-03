c42d0e2dff1a44072ff9e28d8b73a6648d8f8b01
kcsahoo123 <122529250+kcsahoo123@users.noreply.github.com>
2023-08-03 08:50:28 +0100

    change

--- RACE2/WebApiDeployDEV.ps1
+++ RACE2/WebApiDeployDEV.ps1
@@ -1,16 +1,17 @@
 $RESOURCE_GROUP = "race2projectrg"
-$LOCATION="westeurope"
-$CONTAINERAPPS_ENVIRONMENT="race2containerappenv"
-$CONTAINERAPPNAME ="race2frontendweb"
-$REMOTE_IMAGENAME="race2acr.azurecr.io/race2frontendweb:v1"
-$MANAGEDIDENTITY="Race2ManagedIdentity"
-$REGISTRY_SERVER="race2acr.azurecr.io"
-$APPCONFIG_URL="https://race2appconfig.azconfig.io/"
+$RESOURCE_GROUP = "DEVRACINFRG1401"
+$ENVIRONMENT="Production"
+$MANAGEDIDENTITY="DEVRACINFMI1401"
+$APPCONFIG_URL="https://devracinfac1401.azconfig.io/"
+$REGISTRY_SERVER="devracinfcr1401.azurecr.io"
+$CONTAINERAPPS_ENVIRONMENT="DEVRACINFAE1401"
+$CONTAINERAPPNAME ="race2webapi"
+$REMOTE_IMAGENAME="devracinfcr1401.azurecr.io/race2webapi:latest"
 
 $identityClientId = (az identity show --resource-group $RESOURCE_GROUP --name $MANAGEDIDENTITY --output json --query "clientId")
 $identityResourceId = (az identity show --resource-group $RESOURCE_GROUP --name $MANAGEDIDENTITY --output json --query "id")
 
- az containerapp create `
+az containerapp create `
   --name $CONTAINERAPPNAME `
   --resource-group $RESOURCE_GROUP `
   --environment $CONTAINERAPPS_ENVIRONMENT `
@@ -21,7 +22,8 @@ $identityResourceId = (az identity show --resource-group $RESOURCE_GROUP --name 
   --ingress 'external' `
   --cpu 0.5 `
   --memory 1.0Gi `
-  --min-replicas 0 `
+  --min-replicas 1 `
   --max-replicas 2 `
   --env-vars ASPNETCORE_ENVIRONMENT=$ENVIRONMENT AzureAppConfigURL=$APPCONFIG_URL AZURE_CLIENT_ID=$identityClientId `
   --user-assigned $MANAGEDIDENTITY
+ 
