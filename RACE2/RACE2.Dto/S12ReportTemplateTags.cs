b110f8599e9bc7ce41f921c6a59724fa8c471a24
kcsahoo123 <122529250+kcsahoo123@users.noreply.github.com>
2023-09-04 11:23:21 +0100

    ch

--- RACE2/RACE2.Dto/S12ReportTemplateTags.cs
+++ RACE2/RACE2.Dto/S12ReportTemplateTags.cs
@@ -8,7 +8,7 @@ namespace RACE2.Dto
 {
     public class S12ReportTemplateTags
     {
-        public List<String> Tags { get; set; }
+        public List<String> Tags { get; set; } = new List<string>();
         public S12ReportTemplateTags() {
             Tags.Add("Reservoir Name");
             Tags.Add("Supervising Engineer Name");
