cef9e764eb346dc5af32ed8fd6a371c6e562c34d
kcsahoo123 <kriss.sahoo@capgemini.com>
2023-10-04 14:51:52 +0100

    ch

--- RACE2/RACE2.SecurityProvider/Areas/Identity/Pages/Account/ChangeYourPassword.cshtml.cs
+++ RACE2/RACE2.SecurityProvider/Areas/Identity/Pages/Account/ChangeYourPassword.cshtml.cs
@@ -35,6 +35,8 @@ namespace RACE2.SecurityProvider.Areas.Identity.Pages.Account
             _config = config;
         }
 
+        public string WebAppUrl { get; set; }
+
         /// <summary>
         ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
         ///     directly from your code. This API may change or be removed in future releases.
@@ -86,6 +88,7 @@ namespace RACE2.SecurityProvider.Areas.Identity.Pages.Account
 
         public async Task<IActionResult> OnGetAsync()
         {
+            WebAppUrl = _config["RACE2FrontEndURL"];
             var user = await _userManager.GetUserAsync(User);
             if (user == null)
             {
@@ -115,7 +118,7 @@ namespace RACE2.SecurityProvider.Areas.Identity.Pages.Account
                     return Page();
                 }
             }
-
+            WebAppUrl = _config["RACE2FrontEndURL"];
             var user = await _userManager.GetUserAsync(User);
             if (user == null)
             {
