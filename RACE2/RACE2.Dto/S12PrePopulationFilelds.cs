5fc9303e36eb97291939f95aafdaeaf911d08d6f
kcsahoo123 <122529250+kcsahoo123@users.noreply.github.com>
2023-10-12 10:59:36 +0100

    Update S12PrePopulationFilelds.cs

--- RACE2/RACE2.Dto/S12PrePopulationFilelds.cs
+++ RACE2/RACE2.Dto/S12PrePopulationFilelds.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +12,7 @@ namespace RACE2.Dto
         public string ReservoirNearestTown { get; set; }
         public string ReservoirGridRef { get; set; }
         public string SupervisingEngineerName { get; set; }
+        public string SupervisingEngineerCompanyName { get; set; }
         public string SupervisingEngineerAddress { get; set; }
         public string SupervisingEngineerEmail { get; set; }
         public string SupervisingEngineerPhoneNumber { get; set; }
