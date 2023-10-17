b4ec21120b7732f012f99734523a90c726bb9dcc
kcsahoo123 <kriss.sahoo@capgemini.com>
2023-10-17 19:42:40 +0100

    ch

--- RACE2/RACE2.FrontEndWebServer/Pages/S12Pages/ConfirmForgotPasswordEmail.razor.cs
+++ RACE2/RACE2.FrontEndWebServer/Pages/S12Pages/ConfirmForgotPasswordEmail.razor.cs
@@ -13,30 +13,9 @@ namespace RACE2.FrontEndWebServer.Pages.S12Pages
     {
         [Inject]
         public NavigationManager NavigationManager { get; set; } = default!;
-        [Inject]
-        public IUserService userService { get; set; } = default!;
-        private string UserName { get; set; } = "Unknown";
-        private UserDetail UserDetail { get; set; } = default!;
 
         protected override async void OnInitialized()
         {
-            AuthenticationState authState = await AuthenticationStateProvider.GetAuthenticationStateAsync();
-            UserName = authState.User.Claims.ToList().FirstOrDefault(c => c.Type == "name").Value;
-            UserSpecificDto userDetails = await userService.GetUserByEmailID(UserName);
-            UserDetail = new UserDetail()
-            {
-                UserName = UserName,
-                Id = userDetails.Id,
-                Email = userDetails.Email,
-                c_first_name = userDetails.c_first_name,
-                c_last_name = userDetails.c_last_name,
-                c_IsFirstTimeUser = userDetails.c_IsFirstTimeUser
-            };
-            if (UserDetail.c_IsFirstTimeUser)
-            {
-                await userService.UpdateFirstTimeUserLogin(userDetails.Email);
-            }
-
             await base.OnInitializedAsync();
         }
 
