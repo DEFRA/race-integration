b9f6a326a168f12519ce4eb3f44e40c0d70849d4
kcsahoo123 <kriss.sahoo@capgemini.com>
2023-10-30 14:51:41 +0000

    ch

---
+++ RACE2/RACE2.SecurityProvider/Areas/Identity/Pages/Account/RedirectToFrontEndApp.cshtml.cs
@@ -0,0 +1,36 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+#nullable disable
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RACE2.SecurityProvider.Areas.Identity.Pages.Account
+{
+    /// <summary>
+    ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+    ///     directly from your code. This API may change or be removed in future releases.
+    /// </summary>
+    [AllowAnonymous]
+    public class RedirectToFrontEndApp : PageModel
+    {
+        private readonly IConfiguration _config;
+
+        public RedirectToFrontEndApp(IConfiguration config)
+        {
+            _config = config;
+        }
+        public string WebAppUrl { get; set; }
+
+        /// <summary>
+        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
+        public IActionResult OnGet()
+        {
+            WebAppUrl = _config["RACE2FrontEndURL"];
+            return Redirect(WebAppUrl);
+        }
+    }
+}
