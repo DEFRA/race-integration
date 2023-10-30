3de31a3de0a1f873bf0900bccd89e97db51e79b2
kcsahoo123 <kriss.sahoo@capgemini.com>
2023-10-30 07:59:32 +0000

    ch

--- RACE2/RACE2.FrontEndWebServer/Pages/S12Pages/ConfirmResetPassword.razor.cs
+++ RACE2/RACE2.FrontEndWebServer/Pages/S12Pages/ConfirmResetPassword.razor.cs
@@ -1,8 +1,5 @@
 ﻿using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Authorization;
-using RACE2.DataModel;
-using RACE2.Dto;
-using RACE2.Services;
 
 namespace RACE2.FrontEndWebServer.Pages.S12Pages
 {
@@ -12,37 +9,18 @@ namespace RACE2.FrontEndWebServer.Pages.S12Pages
         public IConfiguration _config { get; set; } = default!;
         [Inject]
         public NavigationManager NavigationManager { get; set; } = default!;
-        [Inject]
-        public IUserService userService { get; set; } = default!;
         [CascadingParameter]
         public Task<AuthenticationState> AuthenticationStateTask { get; set; }
-        private string UserName { get; set; } = "Unknown";
-        private UserDetail UserDetail { get; set; } = default!;
         protected override async void OnInitialized()
         {
             AuthenticationState authState = await AuthenticationStateTask; // AuthenticationStateProvider.GetAuthenticationStateAsync();
-            UserName = authState.User.Claims.ToList().FirstOrDefault(c => c.Type == "name").Value;
-            UserSpecificDto userDetails = await userService.GetUserByEmailID(UserName);
-            UserDetail = new UserDetail()
-            {
-                UserName = UserName,
-                Id = userDetails.Id,
-                Email = userDetails.Email,
-                cFirstName = userDetails.cFirstName,
-                cLastName = userDetails.cLastName,
-                cIsFirstTimeUser = userDetails.cIsFirstTimeUser
-            };
-            if (UserDetail.cIsFirstTimeUser)
-            {
-                await userService.UpdateFirstTimeUserLogin(userDetails.Email);
-            }
             await base.OnInitializedAsync();
         }
 
         public void GoToNextPage()
         {
             bool forceLoad = true;
-            string pagelink = "/annual-statements";
+            string pagelink = "/login";
             NavigationManager.NavigateTo(pagelink, forceLoad);
         }
     }
