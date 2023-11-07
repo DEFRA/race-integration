e77198ea8b73b915e85944b2bd73c79d2f8ff097
Mahalakshmi Alagarsamy <mahalakshmi.alagarsamy@capgemini.com>
2023-11-07 10:39:06 +0000

    Update datamodel

--- RACE2/RACE2.DataModel/Reservoir.cs
+++ RACE2/RACE2.DataModel/Reservoir.cs
@@ -19,7 +19,7 @@ namespace RACE2.DataModel
         public string? RaceReservoirId { get; set; }
         [StringLength(200)]
         public string? PublicName { get; set; }
-        [StringLength(200)]
+        [StringLength(200),Required]
         public string? RegisteredName { get;set; }
         [StringLength(8)]
         public string? ReferenceNumber { get; set; }
@@ -29,30 +29,30 @@ namespace RACE2.DataModel
         public string? RegisteredCategory { get;set; }
         [StringLength(12)]
         public string? GridReference { get; set; }       
-        public int Capacity { get; set; }
-        public int SurfaceArea { get; set; }
-        public decimal TopWaterLevel { get; set; }
-        public bool HasMultipleDams { get; set; }
+        public int? Capacity { get; set; }
+        public int? SurfaceArea { get; set; }
+        public decimal? TopWaterLevel { get; set; }
+        public bool? HasMultipleDams { get; set; }
         [StringLength(512)]
         public string? KeyFacts { get; set; }
-        public DateTime ConstructionStartDate { get; set; }
-        public DateTime VerifiedDetailsDate { get; set; }
-        public DateTime LastInspectionDate { get; set; }
+        public DateTime? ConstructionStartDate { get; set; }
+        public DateTime? VerifiedDetailsDate { get; set; }
+        public DateTime? LastInspectionDate { get; set; }
 
         public UserDetail? LastInspectionByUser { get; set; }
         [StringLength(250)]
         public string? LastInspectionEngineerName { get; set; }
         [StringLength(64)]
         public string? LastInspectionEngineerPhone { get; set; }
-        public DateTime LastCertificationDate { get; set; }
+        public DateTime? LastCertificationDate { get; set; }
 
-        public DateTime NextInspectionDate102 { get; set; }
+        public DateTime? NextInspectionDate102 { get; set; }
 
-        public DateTime NextInspectionDate103 { get; set; }
+        public DateTime? NextInspectionDate103 { get; set; }
 
       //  public List<UserDetail> LastInspectionByUser { get; set; } = new List<UserDetail>();
 
-        public Address? Address { get; set; } = new Address();
+       // public Address? Address { get; set; } = new Address();
         public string? NearestTown { get; set; }
        
        public List<UserReservoir> UserReservoirs { get; set; } = new List<UserReservoir>();
