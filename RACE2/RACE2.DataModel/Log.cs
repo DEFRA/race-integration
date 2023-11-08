5cbca96b37b9e67ff64fe320a8d312361db332e5
kcsahoo123 <kriss.sahoo@capgemini.com>
2023-11-08 14:30:42 +0000

    ch

--- RACE2/RACE2.DataModel/Log.cs
+++ RACE2/RACE2.DataModel/Log.cs
@@ -17,12 +17,14 @@ namespace RACE2.DataModel
         public int Id { get; set; }
         public string Message { get; set; }
         public string MessageTemplate { get; set; }
-        public string Level { get; set; }  
+        public string Level { get; set; } 
 
         public DateTime TimeStamp { get; set; }
         public string Exception { get; set; }
-
         public XmlDataType Properties { get; set; }
         public string LogEvent { get; set; }
+        public string UserName { get; set; }
+        public string IP { get; set; }
+
     }
 }
