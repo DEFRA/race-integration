7783b45450d53cb05dd55fad1ff45e12bc28fb66
kcsahoo123 <122529250+kcsahoo123@users.noreply.github.com>
2023-12-01 08:07:31 +0000

    ch

---
+++ RACE2/RACE2.SecurityAzureFunction/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Hosting;
+using RACE2.SecurityAzureFunction;
+
+[assembly: WebJobsStartup(typeof(StartUp))]
+
+public class StartUp : IWebJobsStartup
+{
+    public void Configure(IWebJobsBuilder builder)
+    {
+
+
+    }
+}
