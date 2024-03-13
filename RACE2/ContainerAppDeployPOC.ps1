bdfaf02930cbc7ce8a2fa8fec2fa605386f09c44
kcsahoo123 <122529250+kcsahoo123@users.noreply.github.com>
2024-03-13 09:27:14 +0000

    ch

--- RACE2/ContainerAppDeployPOC.ps1
+++ RACE2/ContainerAppDeployPOC.ps1
@@ -1,6 +1,6 @@
 az containerapp env create `
-  --name race2containerappenv `
-  --resource-group race2projectrg `
-  --location westeurope `
-  --logs-workspace-id 92b64c2e-287a-423c-b3fe-14198a7f3fd5 `
-  --logs-workspace-key 7i/WcjgjAq3sW0SyUtYvsyvEvVPcsmqtGYP5WOshZuSi5NhEyMoQNidaThq3nLYvpmQNR2RlRZbXW0s+qzKYoQ==
+  --name POCRACINFAE1401 `
+  --resource-group POCRACINFRG1401 `
+  --location uksouth `
+  --logs-workspace-id 35cfb691-df31-4bd8-aa9f-6f3d12434e97 `
+  --logs-workspace-key qCnT2sHUtT0tRfJ3krySO+Hs5bPzeshaKgABxsSZSxIHWoqyEas2+s8qnq43CscG354bJ4MlAPuprBz/zKF76A==
