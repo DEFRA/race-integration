0337f5b5af20355b34c128a96e55f2e8fb0fbc65
Mahalakshmi Alagarsamy <mahalakshmi.alagarsamy@capgemini.com>
2024-04-22 12:43:40 +0100

    Updated API

---
+++ RACE2/RACE2.Services/IFileService.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.VisualBasic.FileIO;
+using RACE2.Dto;
+using System.Runtime.CompilerServices;
+
+namespace RACE2.BackendAPIIntegration.Services
+{
+    public interface IFileService
+    {
+        public Task PostFileAsync(IFormFile fileData, FileType fileType);
+
+        //public Task PostMultiFileAsync(List<DocumentDetails> fileData);
+
+        //public Task DownloadFileById(int fileName);
+    }
+
+   
+}
