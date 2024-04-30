64ec395deb4725525f3990bad73f590d5d7236cc
kcsahoo123 <122529250+kcsahoo123@users.noreply.github.com>
2024-04-30 12:42:01 +0100

    Race-1806 changes

--- RACE2/RACE2.FrontEndWebServer/wwwroot/js/utilityfunctions.js
+++ RACE2/RACE2.FrontEndWebServer/wwwroot/js/utilityfunctions.js
@@ -51,5 +51,7 @@ function initializeInactivityTimer(dotnetHelper) {
 
 function hideLink() {
     //document.body.scrollTo(0, document.getElementsByTagName("h1").offsetTop);
-    document.getElementById("skip-to-main-content").style.cssText = "visibility:hidden;";    
+    document.getElementById("skip-to-main-content").style.cssText = "visibility:hidden;"; 
+    return false;
 }
+
