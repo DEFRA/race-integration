8476fd1fe694581578d187b3ac25b2b28667172e
Mahalakshmi Alagarsamy <mahalakshmi.alagarsamy@capgemini.com>
2024-05-16 11:19:17 +0100

    Updated the Dto

---
+++ RACE2/RACE2.BackendAPIIntegration/Controllers/UserController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RACE2.BackendAPIIntegration.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ RACE2/RACE2.BackendAPIIntegration/Models/UserDetails.cs
@@ -0,0 +1,19 @@
+﻿using RACE2.Dto;
+using System.ComponentModel.DataAnnotations;
+
+namespace RACE2.BackendAPIIntegration.Models
+{
+    public class UserDetails : IntegrateUserDTO
+    {
+      public List<IntegrateOrganisationdto> Organisation { get; set; } = new List<IntegrateOrganisationdto>();
+      
+        public List<IntegrateIndividualAddressDetails>  userAddresses { get; set; } = new List<IntegrateIndividualAddressDetails>();
+
+        public List<IntegrateOrganisatioAddressdto> OrganisationAddress { get; set; } = new List<IntegrateOrganisatioAddressdto>();
+        public List<IntegrateMembershipDetails> userMembership { get; set; } = new List<IntegrateMembershipDetails>();
+
+
+      
+      
+    }
+}
