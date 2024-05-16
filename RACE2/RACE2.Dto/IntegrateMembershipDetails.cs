0dc8e360acb2bfb07cde4a4529af38b48bfaeb76
Mahalakshmi Alagarsamy <mahalakshmi.alagarsamy@capgemini.com>
2024-05-16 10:21:55 +0100

    Added the dto for user endpoints

---
+++ RACE2/RACE2.Dto/IntegrateMembershipDetails.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RACE2.Dto
+{
+    public class IntegrateMembershipDetails
+    {
+        public int frontEndPanelMembershipId {  get; set; }
+        [MaxLength(16)]
+        public string backEndPanelMembershipId { get; set; } = string.Empty;
+        [MaxLength(64)]
+        public string panelName {  get; set; } = string.Empty;
+        public DateTime startDate { get; set; }
+        public DateTime endDate { get; set; }
+        [MaxLength(64)]
+        public string status { get; set; } = string.Empty ;
+    }
+}
