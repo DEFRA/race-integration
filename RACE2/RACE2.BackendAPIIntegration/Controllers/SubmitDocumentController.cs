618ec3e947cd0df411586317fbd7cad3ed5859be
kcsahoo123 <122529250+kcsahoo123@users.noreply.github.com>
2024-05-19 08:14:50 +0100

    update

---
+++ RACE2/RACE2.BackendAPIIntegration/Controllers/SubmitDocumentController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RACE2.BackendAPIIntegration.Services;
+using RACE2.Dto;
+using RACE2.Services;
+
+
+namespace RACE2.BackendAPIIntegration.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SubmitDocumentController : Controller
+    {
+        private readonly IRACEIntegrationService _uploadService;
+        private readonly ILogger<SubmitDocumentController> _logger;
+
+        public SubmitDocumentController(IRACEIntegrationService uploadService,ILogger<SubmitDocumentController> logger)
+        { 
+            _uploadService = uploadService;
+            _logger = logger;
+        }
+    
+
+        [HttpPost("SubmitStatement")]
+        public async Task<IntegrationResponseModel> SubmitStatement([FromBody]AnnualSubmissionDocumentDetails submitS12Statement)
+        {
+            IntegrationResponseModel integrationResponseModel = new IntegrationResponseModel();
+            if (submitS12Statement == null)
+            {
+                 integrationResponseModel.StatusCode = System.Net.HttpStatusCode.BadRequest;
+
+                return integrationResponseModel;
+            }
+
+            try
+            {
+                integrationResponseModel =  await _uploadService.SubmitDocumentToBackend(submitS12Statement);
+
+                return integrationResponseModel;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ RACE2/RACE2.BackendAPIIntegration/Program.cs
@@ -0,0 +1,85 @@
+using Microsoft.OpenApi.Models;
+using RACE2.BackendAPIIntegration.Authentication;
+using RACE2.DataAccess.Repository;
+using RACE2.Services;
+using Microsoft.Extensions.Configuration;
+using Azure.Identity;
+using Microsoft.Extensions.Configuration.AzureAppConfiguration;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.AddAzureAppConfiguration(options =>
+{
+    //var connectionString = builder.Configuration["AZURE_APPCONFIGURATION_CONNECTIONSTRING"];
+    var azureAppConfigUrl = builder.Configuration["AzureAppConfigURL"];
+    var azureTenantId = builder.Configuration["AZURE_TENANT_ID"];
+    var managedIdentityClientId = builder.Configuration["ManagedIdentityClientId"]; 
+    var credential = new DefaultAzureCredential(new DefaultAzureCredentialOptions { TenantId = azureTenantId, ManagedIdentityClientId = managedIdentityClientId, VisualStudioTenantId = azureTenantId });
+
+    //options.Connect(connectionString)      
+    options.Connect(new Uri(azureAppConfigUrl), credential)
+    .ConfigureKeyVault(options =>
+    {
+        options.SetCredential(credential);
+    })
+    .ConfigureRefresh(refreshOptions =>
+            refreshOptions.Register("refreshAll", refreshAll: true))
+    .Select(KeyFilter.Any, LabelFilter.Null)
+    // Override with any configuration values specific to current hosting env
+    .Select(KeyFilter.Any, Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT"))
+    .UseFeatureFlags();
+});
+var blazorClientURL = builder.Configuration["RACE2FrontEndURL"];
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "ServiceName", Version = "1" });
+    c.AddSecurityDefinition("ApiKey", new OpenApiSecurityScheme
+    {
+        Name = "x-api-key",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Description = "Authorization by x-api-key inside request's header",
+        Scheme = "ApiKeyScheme"
+    });
+
+    var key = new OpenApiSecurityScheme()
+    {
+        Reference = new OpenApiReference
+        {
+            Type = ReferenceType.SecurityScheme,
+            Id = "ApiKey"
+        },
+        In = ParameterLocation.Header
+    };
+
+    var requirement = new OpenApiSecurityRequirement { { key, new List<string>() } };
+    c.AddSecurityRequirement(requirement);
+});
+
+builder.Services.AddScoped<ApiKeyAuthFilter>();
+
+builder.Services.AddTransient<IRACEIntegrationRepository, RACEIntegrationRepository>();
+builder.Services.AddTransient<IRACEIntegrationService, RACEIntegrationService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
