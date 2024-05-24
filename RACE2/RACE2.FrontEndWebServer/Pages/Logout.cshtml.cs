ab14f3a0a3f129113ae595aa830488e16e5051f7
kcsahoo123 <kriss.sahoo@capgemini.com>
2024-05-24 16:36:32 +0100

    ch

--- RACE2/RACE2.FrontEndWebServer/Pages/Logout.cshtml.cs
+++ RACE2/RACE2.FrontEndWebServer/Pages/Logout.cshtml.cs
@@ -26,47 +26,23 @@ namespace RACE2.FrontEndWebServer.Pages
         var accessToken = await HttpContext.GetTokenAsync("access_token");
         var state = await HttpContext.GetTokenAsync("state");
         var baseUrl = "https://"+Request.Host;
+        string post_logout_redirect_uri = baseUrl;
+        string LogoutAPIurl = "https://oidc.integration.account.gov.uk/logout?id_token_hint={0}&post_logout_redirect_uri={1}";  //&state=af0ifjsldkj
 
+        string requestUri = string.Format(LogoutAPIurl, idToken, post_logout_redirect_uri);
 
-            string post_logout_redirect_uri = baseUrl;
-            string LogoutAPIurl = "https://oidc.integration.account.gov.uk/logout?id_token_hint={0}&post_logout_redirect_uri={1}";  //&state=af0ifjsldkj
+        foreach (var cookie in HttpContext.Request.Cookies.Keys)
+        {
+            HttpContext.Response.Cookies.Delete(cookie);
+        }       
 
-            string requestUri = string.Format(LogoutAPIurl, idToken, post_logout_redirect_uri);
+        //working
+        //Response.Redirect("https://oidc.integration.account.gov.uk/logout");
+        Response.Redirect(requestUri);
 
-           // requestUri += "&Authorization=Bearer "+accessToken;
-
-            foreach (var cookie in HttpContext.Request.Cookies.Keys)
-            {
-                HttpContext.Response.Cookies.Delete(cookie);
-            }
-
-            //var request = new HttpRequestMessage(HttpMethod.Get, requestUri);
-            //request.Headers.Add("Authorization", "Bearer " + accessToken);
-
-            //using (var client = new HttpClient())
-            //{
-            //   // client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
-            //    // client.DefaultRequestHeaders.Add("Authorization", "Bearer " + accessToken);
-            //    client.DefaultRequestHeaders.Add("Authorization", "Bearer " + accessToken);
-            //    using (HttpResponseMessage response = await client.GetAsync(requestUri))
-            //    {
-            //        var responseContent = response.Content.ReadAsStringAsync().Result;
-            //        response.EnsureSuccessStatusCode();
-            //    }
-            //}
-
-
-            //working
-            //  Response.Redirect("https://oidc.integration.account.gov.uk/logout");
-            Response.Redirect(requestUri);
-
-            // try this otherwise
-            //string logoutRedirectUri = "https://oidc.integration.account.gov.uk?id_token_hint=" + idToken + "&post_logout_redirect_uri=" + baseUrl + "&state=sadk8d4--lda%d";
-            //Response.Redirect(logoutRedirectUri);
-
-            //try this otherwise
-            //SignOut(OpenIdConnectDefaults.AuthenticationScheme, CookieAuthenticationDefaults.AuthenticationScheme);
-            //Response.Redirect("/");
-        }
+        //try this otherwise
+        //SignOut(OpenIdConnectDefaults.AuthenticationScheme, CookieAuthenticationDefaults.AuthenticationScheme);
+        //Response.Redirect("/");
+    }
   }
 }
