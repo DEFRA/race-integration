f8f2fde27153e7e294fa53bc1fe09c182b1d8146
kcsahoo123 <kriss.sahoo@capgemini.com>
2024-05-24 17:08:00 +0100

    ch

--- RACE2/RACE2.FrontEndWebServer/Pages/Logout.cshtml.cs
+++ RACE2/RACE2.FrontEndWebServer/Pages/Logout.cshtml.cs
@@ -24,8 +24,8 @@ namespace RACE2.FrontEndWebServer.Pages
 
         var idToken = await HttpContext.GetTokenAsync("id_token");
         var accessToken = await HttpContext.GetTokenAsync("access_token");
-            //var state = await HttpContext.GetTokenAsync("state");
-            string post_logout_redirect_uri = "https://race2frontendwebserver.proudbeach-8eb578a1.uksouth.azurecontainerapps.io";// "https://" + Request.Host;
+        //var state = await HttpContext.GetTokenAsync("state");
+        string post_logout_redirect_uri = "https://race2frontendwebserver.proudbeach-8eb578a1.uksouth.azurecontainerapps.io";// "https://" + Request.Host;
         string LogoutAPIurl = "https://oidc.integration.account.gov.uk/logout?id_token_hint={0}&post_logout_redirect_uri={1}";  //&state=af0ifjsldkj
 
         string requestUri = string.Format(LogoutAPIurl, idToken, post_logout_redirect_uri);
@@ -35,25 +35,9 @@ namespace RACE2.FrontEndWebServer.Pages
             HttpContext.Response.Cookies.Delete(cookie);
         }       
 
-            //var request = new HttpRequestMessage(HttpMethod.Get, requestUri);
-            //request.Headers.Add("Authorization", "Bearer " + accessToken);
-
-            //using (var client = new HttpClient())
-            //{
-            //   // client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
-            //    // client.DefaultRequestHeaders.Add("Authorization", "Bearer " + accessToken);
-            //    client.DefaultRequestHeaders.Add("Authorization", "Bearer " + accessToken);
-            //    using (HttpResponseMessage response = await client.GetAsync(requestUri))
-            //    {
-            //        var responseContent = response.Content.ReadAsStringAsync().Result;
-            //        response.EnsureSuccessStatusCode();
-            //    }
-            //}
-
-
-            //working
-            //  Response.Redirect("https://oidc.integration.account.gov.uk/logout");
-            Response.Redirect(requestUri);
+        //working
+        //Response.Redirect("https://oidc.integration.account.gov.uk/logout");
+        Response.Redirect(requestUri);
 
         //try this otherwise
         //SignOut(OpenIdConnectDefaults.AuthenticationScheme, CookieAuthenticationDefaults.AuthenticationScheme);
