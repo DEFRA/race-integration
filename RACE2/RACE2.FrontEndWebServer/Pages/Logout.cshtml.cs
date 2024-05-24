52e8c1a489a6937319a119333f7dcbd3aae33968
kcsahoo123 <122529250+kcsahoo123@users.noreply.github.com>
2024-05-24 10:24:12 +0100

    ch

--- RACE2/RACE2.FrontEndWebServer/Pages/Logout.cshtml.cs
+++ RACE2/RACE2.FrontEndWebServer/Pages/Logout.cshtml.cs
@@ -16,12 +16,19 @@ namespace RACE2.FrontEndWebServer.Pages
 
         var idToken = await HttpContext.GetTokenAsync("id_token");
         var accessToken = await HttpContext.GetTokenAsync("access_token");
+        var baseUrl = "https://"+Request.Host;
 
         foreach (var cookie in HttpContext.Request.Cookies.Keys)
         {
             HttpContext.Response.Cookies.Delete(cookie);
         }
         Response.Redirect("https://oidc.integration.account.gov.uk/logout");
+
+        // try this otherwise
+        //string logoutRedirectUri = "https://oidc.integration.account.gov.uk?id_token_hint=" + idToken + "&post_logout_redirect_uri=" + baseUrl + "&state=sadk8d4--lda%d";
+        //Response.Redirect(logoutRedirectUri);
+
+        //try this otherwise
         //SignOut(OpenIdConnectDefaults.AuthenticationScheme, CookieAuthenticationDefaults.AuthenticationScheme);
         //Response.Redirect("/");
     }
