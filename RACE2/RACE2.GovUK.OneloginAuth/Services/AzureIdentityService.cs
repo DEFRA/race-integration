eace933685f6ba8fab1055fbaf6e422b941c086a
kcsahoo123 <kriss.sahoo@capgemini.com>
2024-05-27 11:01:02 +0100

    ch

--- RACE2/RACE2.GovUK.OneloginAuth/Services/AzureIdentityService.cs
+++ RACE2/RACE2.GovUK.OneloginAuth/Services/AzureIdentityService.cs
@@ -25,7 +25,12 @@ namespace RACE2.GovUK.OneloginAuth.Services
 
         public async Task<string> AuthenticationCallback(string authority, string resource, string scope)
         {
-            var credential = new DefaultAzureCredential();
+            var azureAppConfigUrl = "https://pocracinfac1402.azconfig.io/";
+            var azureTenantId = "6f504113-6b64-43f2-ade9-242e05780007";
+            var managedIdentityClientId = "80026cc9-216c-4002-8d7c-f4648af0f20f";
+            var credential = new DefaultAzureCredential(new DefaultAzureCredentialOptions { TenantId = azureTenantId, ManagedIdentityClientId = managedIdentityClientId, VisualStudioTenantId = azureTenantId });
+
+            //var credential = new DefaultAzureCredential();
             var token = await credential.GetTokenAsync(
                 new TokenRequestContext(scopes: new[] { "https://vault.azure.net/.default" }));
 
